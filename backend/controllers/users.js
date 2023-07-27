11f6efee7027e855367be8ef33d5a32e61adc726
Elizaveta Muratova <elisiabells@gmail.com>
2023-07-28 02:58:54 +0400

    back

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -25,7 +25,7 @@ module.exports.login = (req, res, next) => {
 
           const token = jwt.sign(
             { _id: user._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
           );
 
           res.cookie('jwt', token, {
